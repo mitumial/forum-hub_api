049b22cba904e2bdf9fd10c47bc22a9c5a5e388f
mitumial <ace.vortex76@gmail.com>
2025-01-19 14:48:16 -0500

    add error handler

---
+++ src/main/java/com/onechallenge/forumhubby/infra/error/ErrorHandler.java
@@ -0,0 +1,14 @@
+package com.onechallenge.forumhubby.infra.error;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity handleError404(){
+        return ResponseEntity.notFound().build();
+    }
+}
