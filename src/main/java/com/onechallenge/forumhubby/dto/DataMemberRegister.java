45fdb07927907d78422541d585942f7feda6d418
mitumial <ace.vortex76@gmail.com>
2025-01-19 23:06:12 -0500

    add validation annotations to DataMemberRegister

--- src/main/java/com/onechallenge/forumhubby/dto/DataMemberRegister.java
+++ src/main/java/com/onechallenge/forumhubby/dto/DataMemberRegister.java
@@ -1,8 +1,15 @@
 package com.onechallenge.forumhubby.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record DataMemberRegister(
+        @NotBlank
         String alias,
+        @NotBlank
+        @Email
         String email,
+        @NotBlank
         String password
 ) {
 }
